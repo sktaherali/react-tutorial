e4cc067c6e3a4fc6f5484ed2b35dfb78d4aed1bc
Sk Taher Ali <sktaherali1997@gmail.com>
2024-04-26 19:32:47 +0530

    test

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 const headingChild = React.createElement("h1", { id: "heading" }, "Hello World 1");
 const headingChild1 = React.createElement("h1", { id: "heading1" }, "Hello World 1");
 
-const heading = React.createElement("div", { id: "heading" },  
+const heading = React.createElement("div", { id: "heading" },  [headingChild,headingChild1]);
 
 console.log(heading.props);
 const root = ReactDOM.createRoot(document.getElementById("root"));
